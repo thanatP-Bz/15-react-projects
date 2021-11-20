181a0a4c2b28820d2960a4f6f5e3fc8206c7fc01
Thanat <bztzn512@gmail.com>
2021-11-19 21:11:30 -0500

    question

--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,23 @@
 import React, { useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({ title, info }) => {
+  const [showInfo, setInfo] = useState(false);
+
+  const onClickHandler = () => {
+    setInfo(!showInfo);
+  };
+
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={onClickHandler}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
