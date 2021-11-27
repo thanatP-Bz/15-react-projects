59e6ae93ee572a8ff7832f5c7a55c109a6cfa87d
Thanat <bztzn512@gmail.com>
2021-11-26 21:07:50 -0500

    lorem

--- src/App.js
+++ src/App.js
@@ -1,67 +1,32 @@
-import React, { useState, useEffect } from "react";
-import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
-import { FaQuoteRight } from "react-icons/fa";
+import React, { useState } from "react";
 import data from "./data";
 
 function App() {
-  const [people, setPeople] = useState(data);
-  const [index, setIndex] = useState(0);
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
 
-  useEffect(() => {
-    const lastIndex = people.length - 1;
-    if (index < 0) {
-      setIndex(lastIndex);
-    }
-    if (index > lastIndex) {
-      setIndex(0);
-    }
-  }, [index, people]);
-
-  useEffect(() => {
-    let slider = setInterval(() => {
-      setIndex(index + 1);
-    }, 3000);
-    return () => clearInterval(slider);
-  }, [index]);
-
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <section className="section">
-      <div className="title">
-        <h2>
-          <span>/</span>review
-        </h2>
-      </div>
-      <div className="section-center">
-        {people.map((person, personIndex) => {
-          const { id, image, name, title, quote } = person;
-
-          let position = "nextSlide";
-          if (personIndex === index) {
-            position = "activeSlide";
-          }
-          if (
-            personIndex === index - 1 ||
-            (index === 0 && personIndex === people.length - 1)
-          ) {
-            position = "lastSlide";
-          }
-          return (
-            <article className={position} key={id}>
-              <img src={image} alt={name} className="person-img" />
-              <h4>{name}</h4>
-              <p className="title">{title}</p>
-              <p className="text">{quote}</p>
-              <FaQuoteRight className="icon" />
-            </article>
-          );
-        })}
-        <button className="prev" onClick={() => setIndex((index) => index - 1)}>
-          <FiChevronLeft />
-        </button>
-        <button className="next" onClick={() => setIndex((index) => index + 1)}>
-          <FiChevronRight />
-        </button>
-      </div>
+    <section className="section-center">
+      <h3>Tired of boring Lorem Ipsum</h3>
+      <form className="lirem-form" onSubmit={handlerSubmit}></form>
+      <label htmlFor="amount">paragraph:</label>
+      <input
+        type="number"
+        name="amount"
+        id="amount"
+        value={count}
+        onChange={(e) => setCount(e.target.value)}
+      />
+      <button type="submit" className="btn">
+        Genetator
+      </button>
+      <article className="lorem-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti alias
+        ipsum perspiciatis earum veritatis ducimus sunt!
+      </article>
     </section>
   );
 }
