a77b9cca8e7307b67947db8e15b04d0a58eab8b8
Thanat <bztzn512@gmail.com>
2021-12-09 22:30:06 -0500

    Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,19 @@
 import React, { useState, useRef, useEffect } from "react";
 import { FaBars, FaTwitter } from "react-icons/fa";
-import { links, social } from "./data";
+import { links, social } from "./Data";
 import logo from "./logo.svg";
 
 const Navbar = () => {
-  return <h4>navbar</h4>;
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">Logo</div>
+        <div className="links-container show-container">
+          <ul className="socoial-icons"></ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
