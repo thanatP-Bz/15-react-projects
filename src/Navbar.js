7b0c5e6a841a6d054bd953ea189c7b086e51c018
Thanat <bztzn512@gmail.com>
2021-12-13 21:38:04 -0500

    navBarFunction

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,35 +1,53 @@
 import React, { useState, useRef, useEffect } from "react";
 import { FaBars, FaTwitter } from "react-icons/fa";
 import { links, social } from "./Data";
-import logo from "./logo.svg";
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = null;
+  const linksRef = null;
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           Logo
-          <button className="nav-toggle">
+          <button
+            className="nav-toggle"
+            onClick={() => setShowLinks(!showLinks)}
+          >
             <FaBars />
           </button>
         </div>
-        <div className="links-container show-container">
-          <div className="links">
-            <li>
-              <a href="#">Home</a>
-              <a href="#">About</a>
-              <a href="#">Contact</a>
-              <a href="#">Product</a>
-            </li>
-          </div>
-          <ul className="social-icons">
-            <li>
-              <a href="http://www.twitter.com">
-                <FaTwitter />
-              </a>
-            </li>
+
+        <div className="links-container " ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
           </ul>
         </div>
+        <ul className="social-icons">
+          <li>
+            <a href="http://www.twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="http://www.twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="http://www.twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
